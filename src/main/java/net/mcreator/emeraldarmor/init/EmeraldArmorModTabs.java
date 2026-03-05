7154582ebf568f836d31313cc2f37679479fe43c
Noxysoff <chavistimothee@gmail.com>
2026-03-05 21:22:10 +0100

    Upload

--- src/main/java/net/mcreator/emeraldarmor/init/EmeraldArmorModTabs.java
+++
@@ -1,9 +0,0 @@
-/*
- *    MCreator note: This file will be REGENERATED on each build.
- */
-package net.mcreator.emeraldarmor.init;
-
-public class EmeraldArmorModTabs {
-	public static void load() {
-	}
-}
